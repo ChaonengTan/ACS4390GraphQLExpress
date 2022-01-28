39f993562efa0ac35ea1e06a37c456f3e0c43e05
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-01-27 23:15:29 -0800

    ++
    geolocation
    getWeather
    interface

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -2,17 +2,48 @@ import { useState } from 'react'
 import { gql } from '@apollo/client'
 import { client } from '../index'
 import DisplayWeather from './WeatherDisplay'
+import './Weather.css'
 
 function Weather() {
     const [ zip, setZip ] = useState('')
     const [ weather, setWeather ] = useState(null)
+    const [ units, setUnits ] = useState('standard')
 
+    const [ latitude, setLatitude ] = useState(null)
+    const [ longitude, setLongitude ] = useState(null)
+    navigator.geolocation.getCurrentPosition(position => {
+        // console.log("Latitude is :", position.coords.latitude);
+        // console.log("Longitude is :", position.coords.longitude);
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+    });
+    async function getWeatherGeo() {
+        try {
+            const json = await client.query({
+                query: gql`
+                    query {
+                        getWeather(lat:${latitude}, lon:${longitude}, units:${units}) {
+                            name
+                            temperature
+                            description
+                            pressure
+                            humidity
+                            cod
+                            message
+                        }
+                    }`
+            })
+            setWeather(json)
+        } catch(err) {
+            console.log(err.message)
+        }
+    }
     async function getWeather() {
         try {
             const json = await client.query({
                 query: gql`
                     query {
-                        getWeather(zip:${zip}) {
+                        getWeather(zip:${zip}, units:${units}) {
                             name
                             temperature
                             description
@@ -31,12 +62,22 @@ function Weather() {
 
     return (
         <div className="Weather">
-            {weather ? <DisplayWeather data={weather.data.getWeather}/> : null}
+            {weather ? <DisplayWeather data={weather.data.getWeather} units={units}/> : null}
             <form onSubmit={(e) => {
                 e.preventDefault()
                 getWeather()
             }}>
-                <input value={zip} onChange={(e) => setZip(e.target.value)}/>
+                <label for='zipInput'>ZIP: </label>
+                <input id='zipInput' value={zip} onChange={(e) => setZip(e.target.value)}/>
+                <div onChange={(e) => setUnits(e.target.value)}>
+                    <label>Units | </label>
+                    <label for='standardRadio'>Kelvin:</label>
+                    <input id='standardRadio' type='radio' name='units' value='standard'></input>
+                    <label for='metricRadio'>Celcius:</label>
+                    <input id='metricRadio' type='radio' name='units' value='metric'></input>
+                    <label id='imperialRadio'>Fahrenheit:</label>
+                    <input id='imperialRadio' type='radio' name='units' value='imperial'></input>
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
